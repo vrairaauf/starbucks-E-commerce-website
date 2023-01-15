67b7ba4128ebdd61b5e13abff918bd1ab91530a2
raauf <vrairaaufabidi@gmail.com>
2023-01-15 10:00:37 +0100

    add : create some essential alerts pages -> alert.php is a page to show error message when the use click the add-to-pannel button and he is unauthorised , success_command.php is an success alert that showing when the product added to pannel after clicking on add-to-pannel button and alert_success_add_product_to_pannel.php is a page loading after the user confirm the command -> Resolver #101 Developement method : pair programming With : raauf

---
+++ alert_success_add_product_to_pannel.php
@@ -0,0 +1,3 @@
+<div class="alert alert-success" role="alert">
+  A simple success alert with <a href="?role=customer&tache=pannel" class="alert-link">move to pannel</a>. Coffee added to pannel.
+</div>
